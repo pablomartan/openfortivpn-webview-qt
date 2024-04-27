be2a2dd219ae9747273117fb3cdefed51e248f53
Pablo Martínez <pablomartan@kolabnow.com>
2024-04-27 14:50:34 -0700

    feat: add package derivation

---
+++ openfortivpn-webview-qt/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, ...}:
+
+pkgs.stdenv.mkDerivation rec {
+    pname = "openfortivpn-webview";
+    version = "1.20";
+    owner = "gm-vm";
+    repo = "openfortivpn-webview";
+    rev = "36fd3ea39306152470ab202e9049f282822a0eef";
+
+    src = pkgs.fetchFromGitHub {
+        owner = owner;
+        repo = repo;
+        rev = rev;
+        sha256 = "sha256-BM5hurJDPYpbt2WV6C1dldLx2wD9eDZlTK/TeHXpmY0=";
+    };
+
+    buildInputs = with pkgs.qt6; [ qmake qtwebengine qtwayland ];
+
+    nativeBuildInputs = with pkgs.qt6; [ wrapQtAppsHook ];
+    dontWrapQtApps = false;
+
+    configurePhase = ''
+        cd ./openfortivpn-webview-qt
+    '';
+
+    buildPhase = ''
+        qmake . DESTDIR=$out/bin && make
+    '';
+#
+#    installPhase = ''
+#        make install
+#    '';
+}

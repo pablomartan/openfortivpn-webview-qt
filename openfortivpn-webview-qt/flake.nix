09a78ec0424c86731fbc683b63685343f59160e2
Pablo Martínez Adán <p.martinez@cleverpy.com>
2025-01-02 15:34:06 +0100

    feat: update flake to last stable nixpkgs version

--- openfortivpn-webview-qt/flake.nix
+++ openfortivpn-webview-qt/flake.nix
@@ -2,7 +2,7 @@
   description = "openfortivpn-webview flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
   outputs = {nixpkgs, ...}: let

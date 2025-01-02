c1324dce4843ac5eb1bbab26df46b98c6dc51bcf
Pablo Martínez Adán <p.martinez@cleverpy.com>
2025-01-02 15:33:05 +0100

    style: run formatter

--- openfortivpn-webview-qt/flake.nix
+++ openfortivpn-webview-qt/flake.nix
@@ -1,15 +1,14 @@
 {
-    description = "openfortivpn-webview flake";
+  description = "openfortivpn-webview flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
 
-    outputs = { self, nixpkgs, ...}:
-    let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-    in {
-        packages.${system}.default = (import ./default.nix { inherit pkgs; });
-    };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = import ./default.nix {inherit pkgs;};
+  };
 }
